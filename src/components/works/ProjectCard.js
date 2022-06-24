46f6d972d9c04fe0062536284e0ead23d2526727
Fatima Ezzahra <felemenoun@gmail.com>
2022-06-24 15:10:43 +0100

    Style project card

--- src/components/works/ProjectCard.js
+++ src/components/works/ProjectCard.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import styles from './ProjectCard.module.scss';
 import budget from '../../images/mybudget.png';
+//import Long from '../../images/long.png';
 
 const ProjectCard = () => (
   <div className={styles.card_container}>
-    <h3>myBudget</h3>
-    <img src={budget} alt="mybugdet" className={styles.project_image} />
-    <p>
-      myBudget is a mobile web application where you can manage your budget:
-      you have a list of transactions associated with a category,
-      so that you can see how much money you spent and on what.
-    </p>
-    <ul>
-      <li>Rails</li>
-      <li>Ruby</li>
-      <li>Sass</li>
-      <li>HTML</li>
-      <li>RSpec</li>
-    </ul>
+    <div className={styles.project_info}>
+      <h3>myBudget</h3>
+      <ul>
+        <li>Rails</li>
+        <li>Ruby</li>
+        <li>Sass</li>
+        <li>HTML</li>
+        <li>RSpec</li>
+      </ul>
+      <p>
+        myBudget is a mobile web application where you can manage your budget:
+        you have a list of transactions associated with a category,
+        so that you can see how much money you spent and on what.
+      </p>
+    </div>
     <div className={styles.links_btn}>
       <button type="button">Source</button>
       <button type="button">Live</button>
     </div>
+    <img src={budget} alt="mybugdet" className={styles.project_image} />
+    {/* <img src={Long} alt="mybugdet" className={styles.project_img} /> */}
   </div>
 );
 
