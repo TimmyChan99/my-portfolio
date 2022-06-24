d7176d69455b84670f294417efc3a6beda942c24
Fatima Ezzahra <felemenoun@gmail.com>
2022-06-24 21:24:35 +0100

    Add resume button

--- src/components/aboutMe/AboutMe.js
+++ src/components/aboutMe/AboutMe.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './AboutMe.module.scss';
 import SkillCard from './SkillCard';
+import resume from '../../assets/Resume.pdf';
+import { BiDownload } from 'react-icons/bi'
 
 const AboutMe = () => (
   <div>
@@ -9,7 +11,8 @@ const AboutMe = () => (
       <h2>About Me</h2>
       <hr />
     </div>
-    <p className={styles.aboutme}>
+		<div className={styles.aboutme}>
+      <p>
       I&apos;m a
       {' '}
       <span>full-stack web developer</span>
@@ -19,7 +22,12 @@ const AboutMe = () => (
       Pair-programming and remote work aficionado.
       Fluent in multiple languages, frameworks, and technologies, and
       capable of ramping up quickly and efficiently.
-    </p>
+      </p>
+		 <div className={styles.resume}>
+			 <a href={resume} download>Resume</a>
+       <BiDownload />
+		 </div>
+		</div>
 		<ul>
 			<SkillCard />
 		</ul>
