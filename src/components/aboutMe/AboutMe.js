0722cd4b976ffa26ddb0ce9108724277486f68f8
Fatima Ezzahra <felemenoun@gmail.com>
2022-06-24 23:43:05 +0100

    Add style to about me section

--- src/components/aboutMe/AboutMe.js
+++ src/components/aboutMe/AboutMe.js
@@ -1,37 +1,42 @@
 import React from 'react';
+import { BiDownload } from 'react-icons/bi';
 import styles from './AboutMe.module.scss';
 import SkillCard from './SkillCard';
 import resume from '../../assets/Resume.pdf';
-import { BiDownload } from 'react-icons/bi'
+import skills from '../../assets/skills';
 
 const AboutMe = () => (
-  <div>
+  <section className={styles.aboutme_section}>
     <div className="headline">
       <hr />
       <h2>About Me</h2>
       <hr />
     </div>
-		<div className={styles.aboutme}>
+    <div className={styles.aboutme}>
       <p>
-      I&apos;m a
-      {' '}
-      <span>full-stack web developer</span>
-      {' '}
-      and open-source enthusiast with a love
-      for clean code and accessible design.
-      Pair-programming and remote work aficionado.
-      Fluent in multiple languages, frameworks, and technologies, and
-      capable of ramping up quickly and efficiently.
+        I&apos;m a
+        {' '}
+        <span>full-stack web developer</span>
+        {' '}
+        and open-source enthusiast with a love
+        for clean code and accessible design.
+        Pair-programming and remote work aficionado.
+        Fluent in multiple languages, frameworks, and technologies, and
+        capable of ramping up quickly and efficiently.
       </p>
-		 <div className={styles.resume}>
-			 <a href={resume} download>Resume</a>
-       <BiDownload />
-		 </div>
-		</div>
-		<ul>
-			<SkillCard />
-		</ul>
-  </div>
+      <div className={styles.resume}>
+        <a href={resume} download>Resume</a>
+        <BiDownload />
+      </div>
+    </div>
+    <ul className={styles.skills_card_list}>
+      { skills.map((skill) => (
+        <li key={skill.id}>
+          <SkillCard skill={skill} />
+        </li>
+      ))}
+    </ul>
+  </section>
 );
 
 export default AboutMe;
--- src/components/aboutMe/SkillCard.js
+++ src/components/aboutMe/SkillCard.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './AboutMe.module.scss';
 
-const SkillCard = props => {
-	return (
-		<div>SkillCard</div>
-	)
-}
+const SkillCard = ({ skill }) => (
+  <div className={styles.skill_card}>
+    <h4>{skill.title}</h4>
+    <ul>
+      {skill.list.map((item) => (
+        <li key={item.name}>
+          <item.icon />
+          <span>{item.name}</span>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 
-SkillCard.propTypes = {}
+SkillCard.propTypes = {
+  skill: PropTypes.oneOfType([PropTypes.object]).isRequired,
+};
 
-export default SkillCard
+export default SkillCard;
